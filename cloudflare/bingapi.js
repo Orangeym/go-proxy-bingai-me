74fa230a3b72e6da045b79530476c7b87e5b8678
ysen <35443462+Orangeym@users.noreply.github.com>
2024-05-11 12:30:58 +0800

    Update bingapi.js

--- cloudflare/bingapi.js
+++ cloudflare/bingapi.js
@@ -2,7 +2,6 @@ const AUTHOR = 'Harry-zklcdc/go-proxy-bingai';
 
 const SPILT = '\x1e';
 const BING_ORIGIN = 'https://www.bing.com';
-const SYDNEY_ORIGIN = 'https://sydney.bing.com';
 
 export const bingchatModel = {
   PRECISE: 'Precise',          // 精准
